53ee726a6dbfd9199181ace52df63f9c55fe50a5
Priscila <priscilapintado66@gmail.com>
2021-04-19 10:20:46 -0400

    chore: removed unused npm packages

--- client/app.js
+++ client/app.js
@@ -1,23 +1,18 @@
 import React from 'react'
 import {ThemeProvider} from '@material-ui/styles'
 import theme from './components/theme'
-import {BrowserRouter, Switch} from 'react-router-dom'
+import {BrowserRouter} from 'react-router-dom'
 import {Navbar} from './components'
 import Routes from './routes'
-import {DndProvider} from 'react-dnd'
-import {HTML5Backend} from 'react-dnd-html5-backend'
 
 const App = () => {
   return (
     <div>
       <ThemeProvider theme={theme}>
-        <DndProvider backend={HTML5Backend}>
-          {' '}
-          <BrowserRouter>
-            <Navbar />
-            <Routes />
-          </BrowserRouter>
-        </DndProvider>
+        <BrowserRouter>
+          <Navbar />
+          <Routes />
+        </BrowserRouter>
       </ThemeProvider>
     </div>
   )
